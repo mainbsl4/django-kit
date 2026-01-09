6f5c3a7bf2ba27c33f2849516036291dc677e8cd
mainbsl4 <mainbsl4@gmail.com>
2026-01-09 16:50:55 +0600

    Add GeneralSettingList view with GET method for retrieving general settings

--- apps/system_settings/views.py
+++ apps/system_settings/views.py
@@ -2,10 +2,25 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import GeneralSetting
 from .serializers import GeneralSettingSerializer
+from rest_framework.permissions import AllowAny
+from rest_framework import status
+from rest_framework.response import Response
 
 # Create your views here.
 
 
 class GeneralSettingList(generics.ListCreateAPIView):
+    permission_classes = [AllowAny]
+
     queryset = GeneralSetting.objects.all()
     serializer_class = GeneralSettingSerializer
+
+    def get(self, request, *args, **kwargs):
+
+        response_data = {
+            "success": True,
+            "status": status.HTTP_200_OK,
+            "message": "General Settings retrieved successfully",
+            "data": self.list(request, *args, **kwargs).data,
+        }
+        return Response(response_data, status=status.HTTP_200_OK)
